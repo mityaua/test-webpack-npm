6d3a0616fa1931d4fb5f0edeef32b79710ba73b5
Dmytro <mityaua@gmail.com>
2021-01-08 18:27:38 +0200

    Add localStorage for input

--- src/js/local.js
+++ src/js/local.js
@@ -1,13 +1,35 @@
-const settings = {
-  theme: 'dark',
-  isAuthenticated: true,
-  options: [1, 2, 3],
-};
+// Находим форму и инпут
+const form = document.querySelector('#js-form');
+const input = document.querySelector('#textarea');
 
-localStorage.setItem('settings', JSON.stringify(settings));
+// Добавляем слушателей событий на форму и на инпут
+form.addEventListener('submit', handleFormSubmit);
+input.addEventListener('input', handleText);
 
-const savedSettings = localStorage.getItem('settings');
+// Вызов функции, которая получает данные из local и подставляет их в инпут
+populateMassege();
 
-if (savedSettings) {
-    const parsedSettings = JSON.parse(savedSettings);
+// Функция для прерывания перезагрузки страницы при отправке, очистки local и очистки инпута
+function handleFormSubmit(event) {
+  event.preventDefault();
+  console.log('Отправили имя');
+  localStorage.removeItem('input-text');
+  event.target.reset();
+}
+
+// Функция для записи инпута в local
+function handleText(event) {
+  const message = event.target.value;
+
+  localStorage.setItem('input-text', message);
+}
+
+// Функция для переноса содержимого local в инпут
+function populateMassege() {
+  const savedMessage = localStorage.getItem('input-text');
+
+  if (savedMessage) {
+    console.log(savedMessage);
+    input.value = savedMessage;
+  }
 }
